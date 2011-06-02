e81f0dfd4f24f325a7c913411e45457754cd06d5
FUKAUMI Naoki <naobsd@gmail.com>
2011-06-02 13:55:35 +0900

    add touchscreen calibration

--- full_kyros7015.mk
+++ full_kyros7015.mk
@@ -22,6 +22,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     device/coby/kyros7015/init.d/10softmac:system/etc/init.d/10softmac \
+    device/coby/kyros7015/init.d/11pointercal:system/etc/init.d/11pointercal \
     device/coby/kyros7015/etc/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
     device/coby/kyros7015/etc/vold.fstab:system/etc/vold.fstab \
     device/coby/kyros7015/etc/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
@@ -40,7 +41,8 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/fonts/MTLmr3m.ttf:system/fonts/MTLmr3m.ttf
 
 PRODUCT_PACKAGES += \
-    tccgetserial
+    tccgetserial \
+    TSCalibration
 
 $(call inherit-product, build/target/product/full.mk)
 
