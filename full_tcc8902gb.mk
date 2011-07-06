0fafcc902e6a1290b1916b0027f1622bac58c1a4
FUKAUMI Naoki <naobsd@gmail.com>
2011-07-06 18:35:38 +0900

    use telechips/common

--- full_tcc8902gb.mk
+++ full_tcc8902gb.mk
@@ -6,6 +6,8 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 $(call inherit-product-if-exists, vendor/telechips/tcc8902gb/device-vendor.mk)
 
+$(call inherit-product, device/telechips/common/common.mk)
+
 DEVICE_PACKAGE_OVERLAYS += device/telechips/tcc8902gb/overlay
 
 
@@ -22,26 +24,10 @@ PRODUCT_COPY_FILES += \
     device/telechips/tcc8902gb/init.rc:root/init.rc
 
 PRODUCT_COPY_FILES += \
-    device/telechips/tcc8902gb/init.d/10softmac:system/etc/init.d/10softmac \
-    device/telechips/tcc8902gb/init.d/11pointercal:system/etc/init.d/11pointercal \
-    device/telechips/tcc8902gb/etc/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
-    device/telechips/tcc8902gb/etc/vold.fstab:system/etc/vold.fstab \
-    device/telechips/tcc8902gb/etc/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+    device/telechips/tcc8902gb/etc/vold.fstab:system/etc/vold.fstab
 
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
 
-PRODUCT_PACKAGES += \
-    tccgetserial \
-    TSCalibration
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_tcc8902gb
 PRODUCT_DEVICE := tcc8902gb
