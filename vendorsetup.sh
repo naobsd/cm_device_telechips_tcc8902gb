9f5d53308f96ba516d7ab05158f5d8816e9ee264
FUKAUMI Naoki <naobsd@gmail.com>
2011-07-18 09:20:56 +0900

    add GB suffix for miScroll and Scroll 7 Capacitive just for consistency, no functional changes are intended

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 add_lunch_combo cyanogen_kyros7015gb-eng
 add_lunch_combo cyanogen_m701gb-eng
 add_lunch_combo cyanogen_m701gb_8k-eng
-add_lunch_combo cyanogen_miscroll_8k-eng
+add_lunch_combo cyanogen_miscrollgb_8k-eng
 add_lunch_combo cyanogen_p7sgb_8k-eng
-add_lunch_combo cyanogen_scroll7c_8k-eng
+add_lunch_combo cyanogen_scroll7cgb_8k-eng
